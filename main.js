efdd3c7da57c63c16a66117505e79d6ba30c0c7c
nyagetiria <nyagetirianaomi@gmail.com>
2025-06-26 17:28:29 +0300

    index.html

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', addLikeListeners);
+function hideError() {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
+}
+function addLikeListeners() {
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach(heart => heart.addEventListener('click', likeHeart));
+}
+function likeHeart(e) { 
+  mimicServerCall()
+  .then(() => {
+    if (e.target.innerText === EMPTY_HEART) {
+      e.target.innerText = FULL_HEART;
+      e.target.classList.add('activated-heart');
+    } else {
+      e.target.innerText = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+    }
+  })
+  .catch((error) => {
+    const errorModal = document.getElementById('modal');
+    const errorMessage = document.getElementById('modal-message');
+    errorModal.classList.remove('hidden');
+    errorMessage.innerText = error;
+    setTimeout(hideError, 5000);
+  });
+}
+
+
 
 
 
@@ -20,6 +50,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
+    }, 3000);
   });
 }
